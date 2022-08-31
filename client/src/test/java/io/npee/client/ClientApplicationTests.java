82ac158704ddd1cba9736a78824dc453e539cefc
npee <jmc4664@gmail.com>
2022-08-31 21:13:26 +0900

    클라이언트 역할 애플리케이션 모듈 추가

---
+++ client/src/test/java/io/npee/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.npee.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
