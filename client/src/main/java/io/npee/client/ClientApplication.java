093db9dad877b608b3096310b9d5ec8c6e961a14
npee <jmc4664@gmail.com>
2022-09-03 15:51:37 +0900

    M1 의존성 추가 및 테스트 제거

--- client/src/main/java/io/npee/client/ClientApplication.java
+++ client/src/main/java/io/npee/client/ClientApplication.java
@@ -1,13 +1,25 @@
 package io.npee.client;
 
-import org.springframework.boot.SpringApplication;
+import java.net.URI;
+import java.time.Duration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
+import org.springframework.web.reactive.socket.client.WebSocketClient;
+import reactor.core.publisher.Mono;
 
-@SpringBootApplication
 public class ClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ClientApplication.class, args);
+        WebSocketClient client = new ReactorNettyWebSocketClient();
+
+        client.execute(
+            URI.create("ws://localhost:8889/event-emitter"),
+            session -> session.send(
+                Mono.just(session.textMessage("event-spring-reactive-client-websocket")))
+                              .thenMany(session.receive().map(WebSocketMessage::getPayloadAsText).log())
+                              .then())
+              .block(Duration.ofSeconds(10L));
     }
 
 }
