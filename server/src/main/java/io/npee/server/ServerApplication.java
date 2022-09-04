dde3b9b2fb9e503912c28028438cdac2f4e71bf9
npee <jmc4664@gmail.com>
2022-09-04 17:18:51 +0900

    import 정리

--- server/src/main/java/io/npee/server/ServerApplication.java
+++ server/src/main/java/io/npee/server/ServerApplication.java
@@ -1,14 +1,7 @@
 package io.npee.server;
 
-import java.util.HashMap;
-import java.util.Map;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.HandlerMapping;
-import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
-import org.springframework.web.reactive.socket.WebSocketHandler;
 
 @SpringBootApplication
 public class ServerApplication {
